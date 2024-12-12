356a748ebacd9ce74297d02efb9b2684c48cbeb8
mavm0422 <miguelviveros22@hotmail.com>
2024-12-11 19:17:12 -0500

    Spring Initializer

---
+++ src/main/java/com/alurachallege/forohub/Foro_Hub_Alura/ForoHubAluraApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallege.forohub.Foro_Hub_Alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alurachallege/forohub/Foro_Hub_Alura/ForoHubAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallege.forohub.Foro_Hub_Alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
